2e846fa16cea2c01f2b3ec0cd952b635c25124b3
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-02-27 21:59:02 -0300

    add a new kind of component

--- expenses_project/lib/widgets/transaction_list.dart
+++ expenses_project/lib/widgets/transaction_list.dart
@@ -17,42 +17,71 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemCount: transactions.length,
               itemBuilder: (context, index) {
+                // return Card(
+                //   margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                //   elevation: 3,
+                //   child: Row(
+                //     children: [
+                //       Container(
+                //         margin:
+                //             EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                //         decoration: BoxDecoration(
+                //             border: Border.all(color: Colors.purple, width: 2)),
+                //         padding: EdgeInsets.all(10),
+                //         child: Text(
+                //           '\$ ${transactions[index].amount.toStringAsFixed(2)}',
+                //           style: TextStyle(
+                //             fontWeight: FontWeight.bold,
+                //             fontSize: 15,
+                //             color: Theme.of(context).primaryColor,
+                //           ),
+                //         ),
+                //       ),
+                //       Column(
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //         children: [
+                //           Text(transactions[index].title,
+                //               style: Theme.of(context).textTheme.titleMedium),
+                //           Text(
+                //             DateFormat.yMMMMd('en_US')
+                //                 .format(transactions[index].date),
+                //             style: TextStyle(
+                //               color: Theme.of(context).primaryColor,
+                //               fontSize: 10,
+                //             ),
+                //           ),
+                //         ],
+                //       ),
+                //     ],
+                //   ),
+                // );
                 return Card(
-                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  elevation: 3,
-                  child: Row(
-                    children: [
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.purple, width: 2)),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$ ${transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
-                            color: Theme.of(context).primaryColor,
-                          ),
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('\$${transactions[index].amount}'),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(transactions[index].title,
-                              style: Theme.of(context).textTheme.titleMedium),
-                          Text(
-                            DateFormat.yMMMMd('en_US')
-                                .format(transactions[index].date),
-                            style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                              fontSize: 10,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {},
+                    ),
                   ),
                 );
               },
