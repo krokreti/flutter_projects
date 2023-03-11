70566773233c4b41fca06b9ceb8d0177cf2ac6af
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-03-11 09:45:53 -0300

    add fonts

--- my_shop/lib/main.dart
+++ my_shop/lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MyShop',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
       ),
       home: ProductsOverviewScreen(),
     );
