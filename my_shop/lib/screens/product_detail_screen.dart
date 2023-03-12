f69aee4521f9d6c93bfd0d9584768958854f8243
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-03-12 15:45:42 -0300

    add provider

--- my_shop/lib/screens/product_detail_screen.dart
+++ my_shop/lib/screens/product_detail_screen.dart
@@ -3,16 +3,16 @@ import 'package:flutter/material.dart';
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
 
-  final String title;
+  final String id;
 
-  const ProductDetailScreen({Key key, this.title}) : super(key: key);
+  const ProductDetailScreen({Key key, this.id}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
     // ...
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(title: Text('title')),
     );
   }
 }
---
+++ my_shop/lib/widgets/products_grid.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+import './product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (context, index) => ProductItem(
+          id: products[index].id,
+          imageUrl: products[index].imageUrl,
+          title: products[index].title),
+      // childAspectRatio 3/2 a altura é maior q largura, isso q significa
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
