abbd6f7b240b5bcf709b95e4fb0466ca75ac634c
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-03-14 15:50:33 -0300

    add order provider and clear cart

--- my_shop/lib/main.dart
+++ my_shop/lib/main.dart
@@ -6,6 +6,7 @@ import './screens/product_detail_screen.dart';
 import './screens/cart_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
+import './providers/orders.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (context) => Products()),
         ChangeNotifierProvider(create: (context) => Cart()),
+        ChangeNotifierProvider(create: (context) => Orders()),
       ],
       child: MaterialApp(
         title: 'MyShop',
