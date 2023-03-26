f09082429367c73e6ec5711ee83665da37281e2f
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-03-26 16:37:07 -0300

    Add toggle favorite

--- my_shop/lib/providers/orders.dart
+++ my_shop/lib/providers/orders.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 import 'cart.dart';
 
@@ -23,17 +25,63 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(
-      0,
-      OrderItem(
-        id: DateTime.now().toString(),
-        amount: total,
-        products: cartProducts,
-        dateTime: DateTime.now(),
-      ),
-    );
-
-    notifyListeners();
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url = Uri.parse(
+        'https://flutter-test-5df42-default-rtdb.firebaseio.com/orders.json');
+    final timeStamp = DateTime.now();
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'amount': total,
+            'dateTime': timeStamp.toIso8601String(),
+            'products': cartProducts
+                .map((cartProduct) => {
+                      'id': cartProduct.id,
+                      'title': cartProduct.title,
+                      'quantity': cartProduct.quantity,
+                      'price': cartProduct.price,
+                    })
+                .toList(),
+          }));
+      _orders.insert(
+        0,
+        OrderItem(
+          id: json.decode(response.body)['id'],
+          amount: total,
+          products: cartProducts,
+          dateTime: timeStamp,
+        ),
+      );
+      notifyListeners();
+    } catch (e) {
+      print(e);
+    }
   }
+
+  //   final url = Uri.parse(
+  //     'https://flutter-test-5df42-default-rtdb.firebaseio.com/products.json');
+  // return http
+  //     .post(url,
+  //         body: json.encode({
+  //           'title': product.title,
+  //           'description': product.description,
+  //           'imageUrl': product.imageUrl,
+  //           'price': product.price,
+  //           'isFavorite': product.isFavorite
+  //         }))
+  //     .then((response) {
+  //   print(json.decode(response.body));
+  //   final newProduct = Product(
+  //       id: json.decode(response.body)['name'],
+  //       title: product.title,
+  //       description: product.description,
+  //       price: product.price,
+  //       imageUrl: product.imageUrl);
+  //   _items.add(newProduct);
+  //   // _items.insert(0, newProduct); insere no inicio do array
+  //   notifyListeners();
+  // }).catchError((error) {
+  //   print(error);
+  //   throw error;
+  // });
 }
