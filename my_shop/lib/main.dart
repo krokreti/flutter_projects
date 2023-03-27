64772bcc4eb227b66b3a52157a2ef1f6fbfeaa30
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-03-27 18:25:28 -0300

    enable user authentication

--- my_shop/lib/main.dart
+++ my_shop/lib/main.dart
@@ -7,9 +7,11 @@ import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
+import './providers/auth.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,6 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (context) => Auth()),
         ChangeNotifierProvider(create: (context) => Products()),
         ChangeNotifierProvider(create: (context) => Cart()),
         ChangeNotifierProvider(create: (context) => Orders()),
@@ -29,7 +32,7 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.deepOrange,
           fontFamily: 'Lato',
         ),
-        home: ProductsOverviewScreen(),
+        home: AuthScreen(),
         debugShowCheckedModeBanner: false,
         routes: {
           ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
