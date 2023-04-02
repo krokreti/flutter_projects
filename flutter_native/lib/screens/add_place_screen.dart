abda6407343f061d51fc82e9e79e54a7b7ff1245
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-04-02 01:10:42 -0300

    finish location app

--- flutter_native/lib/screens/add_place_screen.dart
+++ flutter_native/lib/screens/add_place_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/image_input.dart';
+import '../widgets/location_input.dart';
 import 'dart:io';
 import 'package:provider/provider.dart';
 import '../providers/great_places.dart';
@@ -50,7 +51,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     ),
                     SizedBox(height: 10),
                     ImageInput(onSelectImage: _selectImage),
-                    TextButton(onPressed: () {}, child: Text('data'))
+                    SizedBox(height: 10),
+                    LocationInput(),
                   ],
                 ),
               ),
