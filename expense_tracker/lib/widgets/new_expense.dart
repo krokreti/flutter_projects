6d9cc9ba06703210e75b25d5c96aece72ed850aa
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-04-23 22:50:05 -0300

    breaking change: fix input decoration bug

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -8,16 +8,46 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Column(
-        children: const [
+        children: [
           TextField(
             maxLength: 50,
             keyboardType: TextInputType.text,
-            decoration: InputDecoration(label: Text('Title')),
+            decoration: const InputDecoration(label: Text('Title')),
+            controller: _titleController,
+          ),
+          TextField(
+            keyboardAppearance: Brightness.dark,
+            keyboardType: TextInputType.number,
+            decoration:
+                const InputDecoration(prefixText: '\$ ', label: Text('Amount')),
+            controller: _amountController,
+          ),
+          Row(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    // Navigator.of(context).pop();
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel')),
+              ElevatedButton(
+                  onPressed: () {}, child: const Text('Save Expense'))
+            ],
           ),
         ],
       ),
